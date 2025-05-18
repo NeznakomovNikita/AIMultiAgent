d218a043f02bde74424613b380259302f6f733b7
NeznakomovNikita <nikitaneznakomov@gmail.com>
2025-05-19 00:28:09 +0300

    Feat: Add Tailwind CSS linting to frontend CI and ESLint config

--- step_flow/frontend_web/eslint.config.js
+++ step_flow/frontend_web/eslint.config.js
@@ -2,12 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +18,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      tailwindcss,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +26,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...tailwindcss.configs.recommended.rules,
     },
   },
 )
